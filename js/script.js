2dfdd4f291e1fb466e7e82f879722fb9b93c0376
Emma Willmann <emma.anne.81@gmail.com>
2019-09-15 23:30:22 -0500

    added exceeds functionality.  user autocomplete, alerts & message sent via model, settings saved to localstorage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const modalMessage = document.querySelector('.modal-message');
+
 const months = ["Jan", "Feb", "Mar", "April"];
 
 const monthlySales = [16547, 14714, 17288, 14732];
@@ -215,3 +219,210 @@ navDiv.addEventListener('click', e => {
     e.target.parentNode.parentNode.classList.add('active');
   }
 });
+
+
+/*Send Message */
+const customer = document.querySelector('#customer');
+const message = document.querySelector('#message');
+const messageForm = document.querySelector('.message-customer form');
+
+function validCustomer() {
+  if (customer.value.length === 0) {
+    customer.placeholder = "Please provide a customer name";
+    customer.className = "error";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validMessage() {
+  if (message.value.length === 0) {
+    message.placeholder = "Please provide a message";
+    message.className = "error";
+    return false;
+  } else {
+    return true;
+  }
+}
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  let flag = true;
+  console.log(validCustomer(), validMessage())
+  if (!validCustomer()) {
+    flag = false;
+  }
+  if (!validMessage()) {
+    flag = false;
+  }
+  console.log(flag)
+  // if (!validCustomer() || !validMessage()) {
+  //   flag = false;
+  // }
+  if (flag) {
+    modal.classList.add('open');
+    modalMessage.innerHTML = `
+      <p> Message successfully sent! <p>
+    `;
+    message.value = "";
+    customer.value = "";
+    message.className = "";
+    customer.className = ""
+    message.placeholder = "Message for Customer"
+    customer.placeholder = "Search for Customer"
+  }
+});
+
+/*Alert Modal */
+const alerts = [
+  'Alert 1: Don\'t forget to start the sale on Monday.',
+  'Alert 2: Place new supply order before end of week.'
+];
+
+const bell = document.querySelector('.bell');
+bell.addEventListener('click', e => {
+  let message = "";
+  alerts.forEach(alert => {
+    message += `
+      <p class="bell-alert">${alert}</p>
+    `;
+
+  });
+  modal.classList.add('open');
+  modalMessage.innerHTML = message;
+});
+
+
+
+/*MODAL Close*/
+
+modal.addEventListener('click', e => {
+  if (e.target.className === 'modal open' || e.target.className === 'close' || e.target.tagName === 'SPAN') {
+    closeModal()
+  }
+})
+
+document.addEventListener('keyup', e => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+function closeModal() {
+  modal.classList.remove('open');
+  modalMessage.innerHTML = "";
+}
+
+/*Name Match autocomplete*/
+const names = [
+  'Jessica Jones',
+  'Tony Stark',
+  'Carol Danvers',
+  'Stephen Strange',
+  'Peter Parker',
+  'Wanda Maximoff',
+  'Clinton Barton',
+  'Steve Rogers'
+];
+
+const autocomplete = document.querySelector('.autocomplete');
+const autoUL = document.querySelector('.autocomplete ul');
+
+
+customer.addEventListener('keyup', e => {
+  if (customer.value.length === 0) {
+    autocomplete.classList.remove('active');
+  } else {
+    let matched = [];
+    names.forEach(name => {
+      console.log(name.toLowerCase(), customer.value.toLowerCase())
+      if (name.toLowerCase().includes(customer.value.toLowerCase())) {
+        matched.push(name);
+      }
+    });
+    if (matched.length > 0) {
+      let list = ""
+      matched.forEach(match => {
+        list += `
+          <li>${match}</li>
+        `
+      });
+      autocomplete.innerHTML = list;
+      autocomplete.classList.add('active')
+    }
+    console.log(matched);
+  }
+});
+
+
+autocomplete.addEventListener('click', e => {
+  if (e.target.tagName === 'LI') {
+    customer.value = e.target.innerText;
+    autocomplete.classList.remove('active');
+  }
+})
+
+
+/*SETTINGS*/
+
+const save = document.querySelector('.save');
+const cancel = document.querySelector('.cancel');
+const email = document.querySelector('#email');
+const profile = document.querySelector('#profile');
+const timeZone = document.querySelector('#timezone');
+console.log(email.checked, profile.value, timeZone.value)
+
+save.addEventListener('click', e => {
+  e.preventDefault();
+  let settings = {};
+  settings.email = email.checked;
+  settings.profile = profile.checked;
+  settings.timeZone = timeZone.value;
+  window.localStorage.setItem('settings', JSON.stringify(settings));
+});
+
+function settings() {
+  let settings = JSON.parse(window.localStorage.getItem('settings'));
+  email.checked = settings.email;
+  profile.checked = settings.profile;
+  timeZone.value = settings.timeZone;
+  console.log(settings);
+}
+
+cancel.addEventListener('click', e => {
+  e.preventDefault();
+  if (window.localStorage.getItem('settings')) {
+    settings();
+  } else {
+    email.checked = false;
+    profile.checked = false;
+    timeZone.value = 'GMT Greenwich Mean Time GMT';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', e => {
+  settings();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
